296bb0f18fc88afd30d481a3a5619a222787b004
Felipe Molina <51002457+FelipeMolinaa@users.noreply.github.com>
2023-08-10 23:57:52 +0000

    mensagem de amor

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 function home(){
-    return "<h1>teste</h1>"
+    return <div>
+        <h1>Oi meu dengo, Só quero que saiba que eu te amo e sempre vou estar aqui com você, independente do que aconteça, eu te amo muito ❤️</h1>
+        <h2>Você é minha musica favorita</h2>
+        <p>- Felipe, Seu amor</p>
+        <p>Aceita namorar comigo?</p>
+        <button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button><button onClick={sim}>sim</button>
+    </div> 
+}
+
+function sim(){
+    alert("ehehehehe tamo namorandooo")
 }
 
 export default home;
