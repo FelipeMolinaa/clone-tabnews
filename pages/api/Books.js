d5f408c24d9a326d95204fa9de8d9164b1b5b78b
Felipe Molina <51002457+FelipeMolinaa@users.noreply.github.com>
2023-09-11 00:10:18 +0000

    [faculdade]

--- pages/api/Books.js
+++ pages/api/Books.js
@@ -1,33 +1,22 @@
 const sqlite3 = require('sqlite3').verbose();
 export default function handler(req,res) {
-    res.status(200).json([
-        {
-        "tituloLivro": "Cem Anos de Solidão",
-        "sumario": "Uma saga familiar mágica e surreal em uma cidade fictícia na América Latina.",
-        "estadoFisico": "Livro usado, em bom estado geral.",
-        "preco": 14.99,
-        "imgBase64": ""
-        },
-        {
-        "tituloLivro": "Harry Potter e a Pedra Filosofal",
-        "sumario": "A história de um jovem bruxo que descobre seu destino no mundo mágico.",
-        "estadoFisico": "Livro usado, com algumas marcas de leitura.",
-        "preco": 9.99,
-        "imgBase64": ""
-        },
-        {
-        "tituloLivro": "A Revolução dos Bichos",
-        "sumario": "Uma fábula que satiriza a política e a corrupção através da história de uma fazenda.",
-        "estadoFisico": "Livro usado, páginas amareladas pelo tempo.",
-        "preco": 7.99,
-        "imgBase64": ""
-        },
-        {
-        "tituloLivro": "O Pequeno Príncipe",
-        "sumario": "Uma história encantadora sobre a amizade entre um príncipe e um aviador.",
-        "estadoFisico": "Livro usado, com assinatura na primeira página.",
-        "preco": 6.99,
-        "imgBase64": ""
+    const db = new sqlite3.Database('meu_banco_de_dados.db');
+
+    db.serialize(() => {
+    // Insira dados
+        db.each('SELECT * FROM Books', (err, row) => {
+            if (err) {
+            console.error(err.message);
+            }
+            res.status(200).json(row)
+        });
+    });
+
+    // Feche o banco de dados quando terminar
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
         }
-    ])
+        console.log('Banco de dados fechado.');
+    });
 }
